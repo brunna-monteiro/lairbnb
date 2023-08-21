3c0cde76743ba6753224ca375780fab8b56b3195
Farah Boughalem <boughalemfarah@gmail.com>
2023-08-21 15:56:24 +0200

    validations

--- app/models/lair.rb
+++ app/models/lair.rb
@@ -1,3 +1,7 @@
 class Lair < ApplicationRecord
   belongs_to :user
+  has_many :users, through: :reservations
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 end
