ac9e7ec1735acf638396a39f044ea767baf68958
Farah Boughalem <boughalemfarah@gmail.com>
2023-08-21 17:09:37 +0200

    add a reservation form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,20 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    raise
+    @reservation = Reservation.new(reservation_params)
+    @lair = Lair.find(params[:lair_id])
+    @reservation.lair = @lair
+    @reservation.user = current_user
+    if @reservation.save!
+      redirect_to lair_path(@lair)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
   end
 end
