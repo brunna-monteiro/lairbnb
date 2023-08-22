d228309e3b199f6394223e18b0a9eab6c447ea25
brunna monteiro <brunnabrauner@gmail.com>
2023-08-22 15:15:20 +0200

    leaving branch to master

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,15 +4,12 @@ export default class extends Controller {
   static targets = ["query"];
 
   connect() {
-    console.log("Hello from search controller");
   }
 
-  submit(event) {
-    event.preventDefault();
-    const query = this.queryTarget.value;
-    const url = this.data.get("url");
+  // submit(event) {
+  //   event.preventDefault();
+  //   const query = this.queryTarget.value;
+  //   const url = this.data.get("url");
 
-    // Redirect to the search results page with the provided query
-    window.location.href = `${url}?query=${encodeURIComponent(query)}`;
-  }
+  // }
 }
